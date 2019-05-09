562253da1e6447c8d0d7a2bd1a1708c6f512deb9
josh-butler <scinewave@gmail.com>
2019-05-08 21:08:28 -0400

    Add test error

--- tapi/app/app.py
+++ tapi/app/app.py
@@ -1,8 +1,15 @@
 import json
 import os
+import logging
+
+log_level = logging.INFO
+logging.basicConfig(level=log_level)
+logger = logging.getLogger()
+logger.setLevel(log_level)
 
 def handler(event, context):
-    print('TABLE_NAME', os.getenv('TABLE_NAME'))
+    logger.error('Testing error alarm')
+
     return {
         "statusCode": 200,
         "body": json.dumps({
