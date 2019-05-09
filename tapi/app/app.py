3ccbd0ab4dd973b7b9a72aae6ba70cc7bc622178
Josh Butler <scinewave@gmail.com>
2019-05-09 09:21:07 -0400

    Update message body

--- tapi/app/app.py
+++ tapi/app/app.py
@@ -3,12 +3,12 @@ import os
 import logging
 
 def handler(event, context):
-    if event.get('httpMethod') == 'GET':
-        raise ValueError('A very specific bad thing happened.')
+    # if event.get('httpMethod') == 'GET':
+    #     raise ValueError('Testing error alarms')
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello SAMMY",
+            "message": "hello world",
         }),
     }
