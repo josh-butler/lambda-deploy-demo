918a01714a787c08d8482061b226a959032c9fc3
josh-butler <scinewave@gmail.com>
2019-05-08 21:20:57 -0400

    raise ValueError

--- tapi/app/app.py
+++ tapi/app/app.py
@@ -2,13 +2,9 @@ import json
 import os
 import logging
 
-log_level = logging.INFO
-logging.basicConfig(level=log_level)
-logger = logging.getLogger()
-logger.setLevel(log_level)
-
 def handler(event, context):
-    logger.error('Testing error alarm')
+    if event.get('httpMethod') == 'GET':
+        raise ValueError('A very specific bad thing happened.')
 
     return {
         "statusCode": 200,
